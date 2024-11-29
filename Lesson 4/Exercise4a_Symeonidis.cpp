dba42274ee8b13d66fa1f821b2c47a9afde54189
Alexandros Symeonidis <symeon59@gmail.com>
2024-11-29 14:55:44 +0200

    Lesson 4 Exercises

--- Lesson 4/Exercise4a_Symeonidis.cpp
+++ Lesson 4/Exercise4a_Symeonidis.cpp
@@ -2,27 +2,15 @@
 using namespace std;
 
 int sub1 (int x);
-int sub3 (int x);
+int add3 (int x);
 int sub5 (int x);
 int mult (int x,int y);
-double mod(int x,int y);
+double division(int x,int y);
+double sum(void);
 
 int main()
 {
-
-    int i=6;
-    double result_temp,result;
-
-    do 
-    {
-        result_temp = mod(mult(sub1(i),sub3(i)),sub5(i));
-        result = result + result_temp;
-        cout << result << endl;
-        i++;
-    }while(result<500);
-
-    cout << "i is equal to " << i-1 << " with result: " << result - result_temp << endl;
-
+    sum();
     return 0;
 }
 
@@ -31,9 +19,9 @@ int sub1 (int x)
     return x-1;
 }
 
-int sub3 (int x)
+int add3 (int x)
 {
-    return x-3;
+    return x+3;
 }
 
 int sub5 (int x)
@@ -46,7 +34,29 @@ int mult (int x,int y)
     return x*y;
 }
 
-double mod(int x,int y)
+double division(int x,int y)
 {
     return (double)x/(double)y;
+}
+
+double sum(void)
+{
+    double result=0.0,result_temp=0.0;
+    int x=6;
+
+    while(1)
+    {
+        result_temp = division(mult(sub1(x),add3(x)),sub5(x));
+        result = result + result_temp;
+        if (result > 500)
+        {
+            break;
+        }
+        cout << "For i=" << x << " the result is: " << result << endl;
+        x++;
+    }
+
+    cout << "For i=" << x+1 << " the result is above 500." << endl;
+
+    return result;
 }
--- Lesson 4/Exercise4b_Symeonidis.cpp
+++ Lesson 4/Exercise4b_Symeonidis.cpp
@@ -2,37 +2,16 @@
 using namespace std;
 
 int sub1 (int *x);
-int sub3 (int *x);
+int add3 (int *x);
 int sub5 (int *x);
-int mult (int *x,int *y);
-double mod(int *x,int *y);
+int mult (int *x);
+double division(int *x);
+double sum(void);
 
 
 int main()
 {
-
-	int x,i=6;
-	double result_temp,result;
-	int temp1,temp3,temp5,temp_mult;
-	double temp_mod;
-
-
-	do
-	{
-	    temp1 = sub1(&i);
-		temp3 = sub3(&i);
-		temp5 = sub5(&i);
-		temp_mult = mult(&temp1,&temp3);
-		temp_mod = mod(&temp_mult,&temp5);
-		
-		result = result + temp_mod;
-		cout << result << endl;
-		i++;
-	} while(result < 500);
-
-	cout << "i is equal to " << i-1 << " with result: " << result - temp_mod << endl;
-
-
+	sum();
 	return 0;
 }
 
@@ -41,9 +20,9 @@ int sub1 (int *x)
 	return *x-1;
 }
 
-int sub3 (int *x)
+int add3 (int *x)
 {
-	return *x-3;
+	return *x+3;
 }
 
 int sub5 (int *x)
@@ -51,12 +30,38 @@ int sub5 (int *x)
 	return *x-5;
 }
 
-int mult (int *x,int *y)
+int mult (int *x)
 {
-	return (*x)*(*y);
+	int y = sub1(x);
+	int z = add3(x);
+	return y*z;
 }
 
-double mod(int *x,int *y)
+double division(int *x)
 {
-	return (double)*x/(double)*y;
+	int y = mult(x);
+	int z = sub5(x);
+	return (double)y/(double)z;
+}
+
+double sum(void)
+{
+    double result=0.0,result_temp=0.0;
+    int x=6;
+
+    while(1)
+    {
+        result_temp = division(&x);
+        result = result + result_temp;
+        if (result > 500)
+        {
+            break;
+        }
+        cout << "For i=" << x << " the result is: " << result << endl;
+        x++;
+    }
+
+    cout << "For i=" << x+1 << " the result is above 500." << endl;
+
+    return result;
 }
